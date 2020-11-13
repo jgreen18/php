ef207e079e2b97787f8d00cb627455f69aed32aa
Javier <jgreen_18@alu.uabcs.mx>
2020-11-12 22:03:10 -0700

    echo

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php $nombre = "Javier"  ?>
+	<?php $lasname = "green" ?>
+
+	<?php 	$fullname = "$nombre $lasname" ?>
+</head>
+<body>
+<?php echo "mi nombre es $fullname" ?>
+
+</body>
+</html>
