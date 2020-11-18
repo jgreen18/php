e3f5773ecc56ea7fd7267b79a7740978243592a6
Javier <jgreen_18@alu.uabcs.mx>
2020-11-17 20:07:53 -0700

    calculadora

--- index.php
+++ index.php
@@ -2,13 +2,51 @@
 <html>
 <head>
 	<title></title>
-	<?php $nombre = "Javier"  ?>
-	<?php $lasname = "green" ?>
-
-	<?php 	$fullname = "$nombre $lasname" ?>
+	
 </head>
 <body>
-<?php echo "mi nombre es $fullname" ?>
+
+
+	<fieldset>
+		 <form method="POST" action="app/test.php">
+		 	<legend>Calculadora en php</legend>
+
+		 	<label>dato 1</label>
+		 	<input type="text" name="n1">
+		 		<br>
+		 		<label>dato 2</label>
+
+		 	<input type="text" name="n2">
+		 	<br>	
+
+
+		 	
+
+		 	<label>
+		 		genero
+		 	</label><br>
+
+		 	<label>+</label>
+		 	<input type="radio" name="operacion" value="+" checked="checked">
+		 	<br>
+		 	<label>-</label>
+		 	<input type="radio" name="operacion" value="-">
+		 	<br>
+		 	<label>/</label>
+		 	<input type="radio" name="operacion" value="/">
+		 	<br>
+		 	<label>*</label>
+		 	<input type="radio" name="operacion" value="*">
+
+		 	<br>
+
+		 	<button type="sumit">
+		 		subir info
+		 	</button>
+
+		 </form>
+	</fieldset>
+
 
 </body>
 </html>
